4d56c39187d789b1fe46da97179d7b25bab09f4e
hieroglyphic0823 <hieroglyphic0823@gmail.com>
2025-08-03 19:57:11 +0900

    テーマ、フォントを変更

--- app/src/main/java/com/example/seedstockkeeper6/ui/theme/Type.kt
+++ app/src/main/java/com/example/seedstockkeeper6/ui/theme/Type.kt
@@ -9,7 +9,12 @@ import androidx.compose.ui.unit.sp
 import com.example.seedstockkeeper6.R
 
 val MPlus1p = FontFamily(
+    Font(R.font.mplus1p_thin, FontWeight.Thin),
+    Font(R.font.mplus1p_light, FontWeight.Light),
     Font(R.font.mplus1p_regular, FontWeight.Normal),
+    Font(R.font.mplus1p_medium, FontWeight.Medium),
+    Font(R.font.mplus1p_bold, FontWeight.Bold),
+    Font(R.font.mplus1p_extrabold, FontWeight.ExtraBold),
     Font(R.font.mplus1p_black, FontWeight.Black)
 )
 
@@ -21,10 +26,34 @@ val AppTypography = Typography(
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
     ),
+    bodyMedium = TextStyle(
+        fontFamily = MPlus1p,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.25.sp
+    ),
+    bodySmall = TextStyle(
+        fontFamily = MPlus1p,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.5.sp
+    ),
     titleLarge = TextStyle(
         fontFamily = MPlus1p,
         fontWeight = FontWeight.Black,
         fontSize = 22.sp
+    ),
+    titleMedium = TextStyle(
+        fontFamily = MPlus1p,
+        fontWeight = FontWeight.Black,
+        fontSize = 18.sp
+    ),
+    titleSmall = TextStyle(
+        fontFamily = MPlus1p,
+        fontWeight = FontWeight.Black,
+        fontSize = 16.sp
     )
     /* Other default text styles to override
     labelSmall = TextStyle(
